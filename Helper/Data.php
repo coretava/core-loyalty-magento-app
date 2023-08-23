0161fa53d85760158f63f85f923c05b8b96a98ce
Riyad Yahya <riyad@coretava.com>
2023-08-23 13:27:46 +0300

    Fix data

--- Helper/Data.php
+++ Helper/Data.php
@@ -2,8 +2,10 @@
 
 namespace Coretava\Loyalty\Helper;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Data extends AbstractHelper
 {
@@ -11,16 +13,24 @@ class Data extends AbstractHelper
     const XML_PATH_CORETAVA = 'loyalty/general/';
     const APP_ID_FIELD = self::XML_PATH_CORETAVA . "coretava_app_id";
     const APP_KEY_FIELD = self::XML_PATH_CORETAVA . "coretava_app_key";
+    protected ScopeConfigInterface $scopeConfig;
+    protected StoreManagerInterface $storeManager;
+
+    public function __construct(ScopeConfigInterface $scopeConfig, StoreManagerInterface $storeManager)
+    {
+        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
+    }
 
     public function getAppId()
     {
         return $this->getConfigValue(self::APP_ID_FIELD);
     }
 
-    public function getConfigValue($field, $storeId = null)
+    public function getConfigValue($field)
     {
         return $this->scopeConfig->getValue(
-            $field, ScopeInterface::SCOPE_STORE, $storeId
+            $field, ScopeInterface::SCOPE_STORE, $this->storeManager->getStore()->getStoreId()
         );
     }
 
